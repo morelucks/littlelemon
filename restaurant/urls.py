cceafd35d683c926d00521357224255ee7c21cd9
morelucks <luckykamshak@gmail.com>
2023-08-24 07:45:01 +0000

    serializers

---
+++ restaurant/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.UserViewSet.as_view , name='userview'),
+  
+]
+
+
+
+
+
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User  # Import the User model
+from rest_framework import permissions  # Import the permissions module
+from .serializers import UserSerializer  # Import the UserSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
